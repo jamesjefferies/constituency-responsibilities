24c397734e4a134b83d91c7853611412472d103e
fantasticlife <michael@smethur.st>
2026-09-13 19:20:12 +0100

    added csv view to organisation overlaps

--- app/controllers/organisation_overlap_controller.rb
+++ app/controllers/organisation_overlap_controller.rb
@@ -21,15 +21,24 @@ class OrganisationOverlapController < ApplicationController
       ] 
     )
     
-    @parent_organisation = Organisation.find( @organisation.parent_organisation_id ) if @organisation.parent_organisation_id
-    
-    @page_title = "#{@organisation.label} - constituency overlaps"
-    @multiline_page_title = "#{@organisation.label} <span class='subhead'>Constituency overlaps</span>".html_safe
-    @description = "#{@organisation.label} constituency overlaps."
-    @crumb << { label: 'Organisations', url: organisation_list_url }
-    @crumb << { label: @organisation.label, url: organisation_show_url }
-    @crumb << { label: 'Overlaps', url: nil }
-    @section = 'organisations'
-    @subsection = 'overlaps'
+    respond_to do |format|
+      format.csv {
+        csv_response_headers( "#{@organisation.label} overlaps" )
+      }
+      format.html {
+      
+        @parent_organisation = Organisation.find( @organisation.parent_organisation_id ) if @organisation.parent_organisation_id
+        
+        @page_title = "#{@organisation.label} - constituency overlaps"
+        @multiline_page_title = "#{@organisation.label} <span class='subhead'>Constituency overlaps</span>".html_safe
+        @description = "#{@organisation.label} constituency overlaps."
+        @csv_url = organisation_overlap_list_url( :format => 'csv' )
+        @crumb << { label: 'Organisations', url: organisation_list_url }
+        @crumb << { label: @organisation.label, url: organisation_show_url }
+        @crumb << { label: 'Overlaps', url: nil }
+        @section = 'organisations'
+        @subsection = 'overlaps'
+      }
+    end
   end
 end
